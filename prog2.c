16e7f6c1a78ae5ea3faf95daa9fe37492b1fdb1a
ejscribner <ejscribner@gmail.com>
2018-09-12 19:44:46 -0400

    reading lines

--- prog2.c
+++ prog2.c
@@ -1,7 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 int main(int argc, char **argv) {
-	printf("Hello world");
+    char* line = NULL;
+    ssize_t lineLength = 0;
+    size_t n = 0;
+    int arg = atoi(argv[1]);
+    int x;
+    if(argc != 2) {
+        fprintf(stderr, "Error: Too Many Arguments \n");
+    } else if(arg < 20 || arg > 100000) {
+        fprintf(stderr, "Error: Invalid Argument Size \n");
+        x = 0;
+    } else {
+        x = arg;
+    }
+    fprintf(stdout, "The number is: %i\n", x);
+    while ((lineLength = getline(&line, &n, stdin)) != -1) {
+        if (lineLength > 0)
+        {
+            if (line[lineLength - 1] == '\n')
+            {
+                line[lineLength - 1] = '\0';
+            }
+        }
+    }
+    fprintf(stdout, "The line is: %s \n", line);
+    free(line);
+    line = NULL;
 	return 0;
 }
