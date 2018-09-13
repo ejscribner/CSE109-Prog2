99b0119d188e7bd5062edb16b3bc3ae94b927bdf
ejscribner <ejscribner@gmail.com>
2018-09-12 20:00:18 -0400

    fixes

--- prog2.c
+++ prog2.c
@@ -6,15 +6,26 @@ int main(int argc, char **argv) {
     char* line = NULL;
     ssize_t lineLength = 0;
     size_t n = 0;
-    int arg = atoi(argv[1]);
+    int arg;
     int x;
-    if(argc != 2) {
+    if(argc == 2) {
+        arg = atoi(argv[1]);
+    } else
+    {
         fprintf(stderr, "Error: Too Many Arguments \n");
-    } else if(arg < 20 || arg > 100000) {
-        fprintf(stderr, "Error: Invalid Argument Size \n");
-        x = 0;
-    } else {
-        x = arg;
+        arg = 0;
+    }
+    if(arg != 0)
+    {
+        if (arg < 20 || arg > 100000)
+        {
+            fprintf(stderr, "Error: Invalid Argument Size \n");
+            x = 0;
+        }
+        else
+        {
+            x = arg;
+        }
     }
     fprintf(stdout, "The number is: %i\n", x);
     while ((lineLength = getline(&line, &n, stdin)) != -1) {
@@ -26,7 +37,7 @@ int main(int argc, char **argv) {
             }
         }
     }
-    fprintf(stdout, "The line is: %s \n", line);
+    fprintf(stdout, "%s\n", line);
     free(line);
     line = NULL;
 	return 0;
