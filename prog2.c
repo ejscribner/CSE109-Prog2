5a42ed29fd4b123cd117eadc430bfeaa13d270f5
ejscribner <ejscribner@gmail.com>
2018-09-17 19:18:14 -0400

    hope this works

--- prog2.c
+++ prog2.c
@@ -2,25 +2,28 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+size_t removeDuplicate(char* line, size_t length, char* duplicate, size_t dupSize);
+size_t* addSize_tElement(size_t* list, size_t* size, size_t* capacity, size_t toAdd);
+
 int main(int argc, char **argv) {
     char* line = NULL;
     ssize_t lineLength = 0;
     size_t n = 0;
-    int arg;
+    int arg ;
     int x;
     if(argc == 2) {
         arg = atoi(argv[1]);
     } else
     {
         fprintf(stderr, "Error: Too Many Arguments \n");
-        arg = 0;
+        return 0;
     }
     if(arg != 0)
     {
         if (arg < 20 || arg > 100000)
         {
             fprintf(stderr, "Error: Invalid Argument Size \n");
-            x = 0;
+            return 0;
         }
         else
         {
@@ -29,16 +32,24 @@ int main(int argc, char **argv) {
     }
     fprintf(stdout, "The number is: %i\n", x);
     while ((lineLength = getline(&line, &n, stdin)) != -1) {
-        if (lineLength > 0)
+        if(lineLength > 0)
         {
-            if (line[lineLength - 1] == '\n')
+            if(line[lineLength - 1] == '\n')
             {
                 line[lineLength - 1] = '\0';
             }
         }
     }
-    fprintf(stdout, "%s\n", line);
+    fprintf(stdout, "The line is: %s\n", line);
     free(line);
     line = NULL;
 	return 0;
+}
+
+size_t removeDuplicate(char* line, size_t length, char* duplicate, size_t dupSize) {
+    return 0;
+}
+
+size_t* addSize_tElement(size_t* list, size_t* size, size_t* capacity, size_t toAdd) {
+    return 0;
 }
