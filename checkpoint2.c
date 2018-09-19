4dcc24aa44503a3fee5424834388e01459a79c50
ejscribner <ejscribner@gmail.com>
2018-09-18 20:49:35 -0400

    stuff

--- checkpoint2.c
+++ checkpoint2.c
@@ -1,38 +1,40 @@
-size_t* addSize_tElement(size_t* list, size_t* size, size_t* capacity, size_t toAdd);
-size_t removeDuplicate(char* line, size_t length, char* duplicate, size_t dupSize);
+/*
+    CSE 109: Fall 2018
+    Elliot Scribner
+    ejs320
+    Program allocates buckets of memory to put words into and prints the result
+    Program #2
+*/
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char **argv)
-{
+size_t *addSize_tElement(size_t *list, size_t *size, size_t *capacity, size_t toAdd);
+size_t removeDuplicate(char *line, size_t length, char *duplicate, size_t dupSize);
 
-}
-
-size_t removeDuplicate(char* line, size_t length, char* duplicate, size_t dupSize)
+size_t removeDuplicate(char *line, size_t length, char *duplicate, size_t dupSize)
 {
     size_t newSize = length - dupSize - 1;
-    size_t j = 0;
-
-    if(strcmp(duplicate, &line[0]) == 0)
+    if (strcmp(duplicate, &line[0]) == 0)
     {
-        while(j < length - dupSize - 1)
+        for (size_t j = 0; j < length - dupSize - 1; j++)
         {
             line[j] = line[j + dupSize + 1];
-            j++;
         }
         return newSize;
     }
-    size_t i = 0;
-    while(i < length)
+
+    for (size_t i = 0; i < length; i++)
     {
-        if(line[i] == '\0' && i != length - 1)
+        if (line[i] == '\0' && i != length - 1)
         {
-            if(strcmp(duplicate, &line[i + 1]) == 0)
+            if (strcmp(duplicate, &line[i + 1]) == 0)
             {
-                size_t j = i;
-                while(j < length - dupSize - 1)
+                for (size_t j = i; j < length - dupSize - 1; j++)
                 {
                     line[j] = line[j + dupSize + 1];
-                    j++;
                 }
                 return newSize;
             }
@@ -41,17 +43,15 @@ size_t removeDuplicate(char* line, size_t length, char* duplicate, size_t dupSiz
     return length;
 }
 
-size_t* addSize_tElement(size_t* list, size_t* size, size_t* capacity, size_t toAdd)
+size_t *addSize_tElement(size_t *list, size_t *size, size_t *capacity, size_t toAdd)
 {
-    if(*size >= *capacity)
+    if (*size >= *capacity)
     {
         size_t newCapacity = (2 * (*capacity)) + 1;
-        size_t* newList = (size_t*)malloc(newCapacity * sizeof(size_t));
-        size_t i = 0;
-        while(i < *size)
+        size_t *newList = (size_t *) malloc(newCapacity * sizeof(size_t));
+        for (size_t i = 0; i < *size; i++)
         {
             newList[i] = list[i];
-            i++;
         }
         free(list);
         *capacity = newCapacity;
