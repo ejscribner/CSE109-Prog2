515108b1efa0aa61f2080f4f112d5ff32a7653dc
ejscribner <ejscribner@gmail.com>
2018-09-22 15:51:00 -0400

    seg fault

--- prog2.c
+++ prog2.c
@@ -12,11 +12,10 @@ char **strsplit(const char *str, const char *delim, size_t *numtokens);
 int main(int argc, char **argv)
 {
     char *line = NULL;
-    size_t lineLength = 0;
+//    size_t lineLength = 0;
 //    size_t n = 0;
-    int arg;
-    int x;
-    size_t numWords = 0;
+    int arg = 100;
+    int bucketSize = 100;
     if (argc == 1)
     {
         arg = 100;
@@ -39,44 +38,64 @@ int main(int argc, char **argv)
         }
         else
         {
-            x = arg;
+            bucketSize = arg;
         }
     }
-    fprintf(stdout, "The number is: %i\n", x);
-    char **words = NULL;
+    fprintf(stdout, "The number is: %i\n", bucketSize);
 
+    char** buckets = NULL;
+    size_t numBuckets = 0;
+    size_t* bucketSizes = 0;
+    size_t* bucketSizesCapacity = 0;
 
-    for (size_t i = 0; i < numWords; i++)
+    ssize_t lineLen = 0;
+    size_t n = 0;
+    while((lineLen = getline(&line, &n, stdin)) != -1)
     {
-        printf("    word is: \"%s\"\n", words[i]);
-//        free(words[i]);
-    }
-    fprintf(stdout, "%s", line);
-    size_t wordLength = 0;
-    char* wordStart = line;
-    while(*line != '\0' && *line != '\n') {
-        while(*line != ' ') {
-            wordLength++;
-            line++;
+        if(lineLen > 0)
+        {
+            if(line[lineLen - 1] == '\n')
+            {
+                line[lineLen - 1] = '\0';
+            }
         }
-        removeDuplicate(line, strlen(line), wordStart, wordLength + 1);
-        wordStart = wordStart + wordLength + 1;
+        fprintf(stdout, "%s \n", line);
+
+
+        char* word;
+        word = strtok(line, " \t");
+        while (word != NULL)
+        {
+            printf ("%s\n", word);
+
+
+            int inserted = 0;
+            size_t wordLength = strlen(word);
+            for(int i = 0; i < numBuckets; i++) {
+                if(bucketSizes[i] + wordLength < bucketSize) {
+                    //put in bucket
+                    inserted = 1;
+                    strcpy(buckets[i], word);
+                }
+            }
+
+            if(inserted == 0) {
+                buckets[numBuckets + 1] = (char*)malloc(bucketSize * sizeof(char));
+                strcpy(buckets[numBuckets + 1], word);
+                bucketSizesCapacity = addSize_tElement(bucketSizes, &wordLength, bucketSizesCapacity, wordLength);
+                numBuckets++;
+            }
+
+            word = strtok (NULL, " \t");
+        }
+
+
+
     }
 
-    fprintf(stdout, "line is: %s \n", line);
-    if (words != NULL)
-    {
-        free(words);
-    }
 
-//    char* bucket = (char*)malloc(x * sizeof(char));
-    int bucketSize = 2;
-    for (int i = 0; i < bucketSize; i++)
-    {
-//        size_t numChars = strlen(buckets[i]) + 1;
-//        fprintf(stdout, "Word %i: %s\n", i, words[i]);
-    }
-//    removeDuplicate(line, lineLength, )
+
+
     free(line);
     line = NULL;
     return 0;
@@ -117,6 +136,24 @@ size_t removeDuplicate(char *line, size_t length, char *duplicate, size_t dupSiz
 
 size_t *addSize_tElement(size_t *list, size_t *size, size_t *capacity, size_t toAdd)
 {
-    return 0;
+    if (*size >= *capacity)
+    {
+        size_t newCapacity = (2 * (*capacity)) + 1;
+        size_t *newList = (size_t *) malloc(newCapacity * sizeof(size_t));
+        for (size_t i = 0; i < *size; i++)
+        {
+            newList[i] = list[i];
+        }
+        free(list);
+        *capacity = newCapacity;
+        newList[*size] = toAdd;
+        (*size)++;
+        return newList;
+    }
+    else
+    {
+        list[*size] = toAdd;
+        (*size)++;
+        return list;
+    }
 }
-
